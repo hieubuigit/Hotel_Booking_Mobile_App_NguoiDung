fbe0a746ca841a4a5330c9f88e1c91192a17bada
unknown <tranminhthuan962@gmail.com>
2020-11-13 21:45:45 +0700

    Chinh sua man hinh ngay 13/11/2020 Thuan

--- app/src/main/java/com/chuyende/hotelbookingappofuser/MainActivity.java
+++ app/src/main/java/com/chuyende/hotelbookingappofuser/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.man_hinh_nha);
+        setContentView(R.layout.man_hinh_quen_mat_khau);
     }
 }
