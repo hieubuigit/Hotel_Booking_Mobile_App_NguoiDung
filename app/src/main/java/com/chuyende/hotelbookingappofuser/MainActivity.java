60cd3e491ee4b2b1d8ce726070d02f3e5bfe46b4
unknown <tranminhthuan962@gmail.com>
2020-11-14 10:58:04 +0700

    Thuan cap nhat 4 man hinh 14/11

--- app/src/main/java/com/chuyende/hotelbookingappofuser/MainActivity.java
+++ app/src/main/java/com/chuyende/hotelbookingappofuser/MainActivity.java
@@ -1,14 +1,37 @@
 package com.chuyende.hotelbookingappofuser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.chuyende.hotelbookingappofuser.activities.frame_ManHinhNha;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.man_hinh_quen_mat_khau);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation); // khai bao layout fragment chính (activity_man layout)
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.farme_container, new frame_ManHinhNha()).commit(); // chạy màn hình nhà dầu9 tiên
     }
+    // click chon bottom navigation
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    androidx.fragment.app.Fragment fragment =null;
+                    switch (item.getItemId()){
+                        case R.id.home:
+                            fragment = new frame_ManHinhNha();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.farme_container, fragment).commit();
+                    return true;
+                }
+            };
 }
